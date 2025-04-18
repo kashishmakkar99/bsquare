57b2d5f02a9669dffbc9a1db55e3b0ec5134037b
Kashish Makkar <51821910+kashishmakkar99@users.noreply.github.com>
2025-04-18 21:08:29 +0530

    Update supbase.ts

--- src/lib/supbase.ts
+++ src/lib/supbase.ts
@@ -1,7 +1,7 @@
 // src/utils/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-id.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseUrl = 'https://ueqlfrgfzqhvyynfiitt.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlcWxmcmdmenFodnl5bmZpaXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5OTAwNzgsImV4cCI6MjA2MDU2NjA3OH0.VXMJaVdlnM_gU3qMkpr9-F2EigfvVeLC33vXtPAvF1U';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
